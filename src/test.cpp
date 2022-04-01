361edeb2c0619c2163b2cbf8c43ab6a038f3a700
Quân <marucube35@gmail.com>
2022-04-01 22:09:10 +0700

    feat: update sample project structure

---
+++ src/test.cpp
@@ -0,0 +1,15 @@
+#include "../include/header.h"
+
+int main()
+{
+    fstream f("../data/sample.txt", ios::in);
+    if (f)
+    {
+        string reader;
+        f >> reader;
+        cout << reader << "\n";
+    }
+
+    system("pause");
+    return 1;
+}
