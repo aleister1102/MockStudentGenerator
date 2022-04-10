b15f84723447a5611569890ddb7e55d17d2df6bf
Lê Minh Quân <marucube35@gmail.com>
2022-04-10 16:28:50 +0700

    chore: cập nhật comment ở test

--- test.cpp
+++ test.cpp
@@ -8,24 +8,24 @@
 
 int main()
 {
-	// Đọc students từ file (task 0)
+	// Đọc students từ file
 	File file;
 	Students students(file.readStudents(STUDENTS2));
 	cout << students.toString() << endl;
-	// Random số n và students (task 1 + 2)
+	// Random số n và students
 	RandomStudents studentsRng;
 	Students randomStudents(studentsRng.next());
 	cout << "----- Random Students -----\n";
 	cout << randomStudents.toString() << endl;
-	// Append vào students và ghi đè vào file (task 3)
+	// Append vào students và ghi đè vào file
 	cout << "----- Students after appending -----\n";
 	students.append(randomStudents);
 	cout << students.toString() << endl;
 	file.writeStudents(STUDENTS2, students.getStudents());
-	// Tính điểm trung bình của students (task 4)
+	// Tính điểm trung bình của students
 	cout << "----- Average Score -----\n";
 	cout << "\t" << students.averageScore() << "\t" << endl;
-	// Tìm students có điểm lớn hơn trung bình (task 5)
+	// Tìm students có điểm lớn hơn trung bình
 	cout << "----- Students have above average scorce -----\n";
 	students.findAboveAvg();
 	system("pause");
